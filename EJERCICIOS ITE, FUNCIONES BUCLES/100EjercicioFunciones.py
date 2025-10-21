6039ce3cece9064c0f9897f6eb1aff7ad4fb17c5
MiguelMartinez32143 <miguelmartinezvaron32143@gmail.com>
2025-10-20 23:15:10 -0500

    Delete EJERCICIOS ITE, FUNCIONES BUCLES directory

--- EJERCICIOS ITE, FUNCIONES BUCLES/100EjercicioFunciones.py
+++
@@ -1,8 +0,0 @@
-# Solución ejercicio 9: promedio de argumentos
-def promedio(*args):
-    if not args:
-        return 0
-    return sum(args) / len(args)
-
-if __name__ == '__main__':
-    print(promedio(2,4,6))
--- EJERCICIOS ITE, FUNCIONES BUCLES/10EjercicioFunciones.py
+++
@@ -1,6 +0,0 @@
-# Solución ejercicio 18: mostrar datos con kwargs
-def mostrar_datos(**datos):
-    return datos
-
-if __name__ == '__main__':
-    print(mostrar_datos(nombre="Ana", edad=20))
